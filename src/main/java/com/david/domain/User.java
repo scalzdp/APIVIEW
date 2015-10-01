1550679ce155079da2ca57dad0119ea496be94bf
scalzdp <275735763@qq.com>
2015-10-01 11:14:53 +0800

    add user details

--- src/main/java/com/david/domain/User.java
+++ src/main/java/com/david/domain/User.java
@@ -4,4 +4,31 @@ package com.david.domain;
  * Created by ZhengDaPeng on 2015/9/30.
  */
 public class User {
+    private  Integer id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
