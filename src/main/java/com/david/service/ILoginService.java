429d1ffb1b83866f689928be0a8156cab3823802
scalzdp <275735763@qq.com>
2015-10-01 11:16:58 +0800

    add Login service

---
+++ src/main/java/com/david/service/ILoginService.java
@@ -0,0 +1,7 @@
+package com.david.service;
+
+/**
+ * Created by Administrator on 2015/10/1.
+ */
+public interface ILoginService {
+}
---
+++ src/main/java/com/david/service/impl/LoginServiceImpl.java
@@ -0,0 +1,9 @@
+package com.david.service.impl;
+
+import com.david.service.ILoginService;
+
+/**
+ * Created by Administrator on 2015/10/1.
+ */
+public class LoginServiceImpl implements ILoginService {
+}
